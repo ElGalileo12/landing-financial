8c5277340abf86c4018df137776aba928c05935a
Kevin Coronado <kevingalileo100@gmail.com>
2025-04-22 18:13:03 -0500

    fix: add suspense in products

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,10 @@
-import ProductosContainer from "@/app/components/productosContainer";
+import Productos from "@/app/products/content/productsContainer";
+import { Suspense } from "react";
 
 export default function ProductosPage() {
   return (
-    <section className="max-w-6xl mx-auto px-4 py-8">
-      <h1 className="text-2xl font-bold mb-6 text-gray-800">Catálogo de Productos</h1>
-      <ProductosContainer />
-    </section>
+    <Suspense fallback={<div>Cargando productos...</div>}>
+      <Productos />
+    </Suspense>
   );
 }
